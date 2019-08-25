e493fbf388e5ccb9f1e19a4a0ef614ca89ec7f75
Tracy Reuther <tracyreuther@gmail.com>
2019-08-25 20:16:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,10 @@
 class Application
 
+# Create a new class array called @@cart to hold any items in your cart
+# Create a new route called /cart to show the items in your cart
+# Create a new route called /add that takes in a GET param with the key item.
+# This should check to see if that item is in @@items and then add it to the
+# cart if it is. Otherwise give an error
   @@items = ["Apples","Carrots","Pears"]
 
   @@cart = []
